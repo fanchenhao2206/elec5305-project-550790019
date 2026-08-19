5d1eacb739860b138b8b9bb09259b3ae93e69029
Tim Fan <hourlilies@icloud.com>
2026-08-19 12:59:33 +1000

    rewrite server using llm; wasm build is buggy?

--- Examples/IPlugInstrument/build-web/server.py
+++ Examples/IPlugInstrument/build-web/server.py
@@ -1,7 +1,11 @@
-import BaseHTTPServer, SimpleHTTPServer
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm'] = 'application/wasm'
+# Set the MIME type for WebAssembly files
+SimpleHTTPRequestHandler.extensions_map['.wasm'] = 'application/wasm'
+
 port = 8000
-httpd = BaseHTTPServer.HTTPServer(('localhost', 8000), SimpleHTTPServer.SimpleHTTPRequestHandler)
-print("Serving at http://localhost:8000/ waiting for connections")
+# Create the server bound to localhost
+httpd = HTTPServer(('localhost', port), SimpleHTTPRequestHandler)
+
+print(f"Serving at http://localhost:{port}/ waiting for connections")
 httpd.serve_forever()
