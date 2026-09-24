019af8a576dad03df1a69b03775e3ba1118ad47b
Tim Fan <fanchenhao2206@icloud.com>
2026-09-24 23:41:56 +1000

    this is the only way i know how to program

--- picoDAW/picoDAW.cpp
+++ picoDAW/picoDAW.cpp
@@ -61,7 +61,7 @@ picoDAW::picoDAW(const InstanceInfo& info)
 
     IRECT sequencerBounds = b.GetReducedFromBottom( // 20px gap above top of keyboard; 640 x 480px box
       160 + 20).GetReducedFromLeft(160).GetReducedFromRight(160).GetReducedFromTop(80 - 20);
-    pGraphics->AttachControl(new IVMultiToggleControl<16>(sequencerBounds, "sequencer"));
+    pGraphics->AttachControl(new IVSequencerControl(sequencerBounds));
   };
 #endif
 }
